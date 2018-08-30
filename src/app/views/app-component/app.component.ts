26a91f41ba8c20a6a6f0e4394be8c18cf08c7a54
Pavenko Stanislav <pavenko_sv@mail.ru>
2018-08-30 18:38:03 +0300

    wip

--- src/app/views/app-component/app.component.ts
+++ src/app/views/app-component/app.component.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 	host: {
-	  '(window:resize)': 'onResize($event)'
+		'(window:resize)': 'onResize($event)'
 	},
 	encapsulation: ViewEncapsulation.None
 })
@@ -16,29 +16,29 @@ export class AppComponent implements OnInit {
 
 	ngOnInit() {
 		this.appSrv.setCurrentTemplate('registration');
-		
-	this._gridSize.asObservable().subscribe(
-		(gridSize: GridSize) => { this.appSrv.GridSize = gridSize; }
-	  );
+
+		this._gridSize.asObservable().subscribe(
+			(gridSize: GridSize) => { this.appSrv.GridSize = gridSize; }
+		);
 	}
 
-	
+
 	// --- Grid ---	
 
 	private _gridSize = new BehaviorSubject<GridSize>('lg');
-	
+
 	initGridSize(width: number): void {
-		if (width < 320) {
-			this._gridSize.next('sm');
-		}
-		if (width < 1200) {
-			this._gridSize.next('lg');
-		}
 		if (width >= 1600) {
 			this._gridSize.next('xl');
+			return;
 		}
+		if (width >= 1200) {
+			this._gridSize.next('lg');
+			return;
+		}
+		this._gridSize.next('sm');
 	}
-	
+
 	onResize(event: any) {
 		this.initGridSize(event.target.innerWidth);
 	}
